550230a9c278d48cc4d26d0a5a33d214aa138451
Adontz <adontz@gmail.com>
2019-04-01 14:27:10 +0400

    Add scheduler test.

---
+++ tests/scheduler_test.py
@@ -0,0 +1,29 @@
+from broomio import Loop
+from broomio import Nursery
+from broomio import sleep
+from math import fabs
+from time import time
+from unittest import main
+from unittest import TestCase
+
+
+class TestScheduler(TestCase):
+    def test_scheduler(self):
+        order = []
+
+        async def child(index):
+            await sleep(index / 100.0)
+            order.append(index)
+
+        loop = Loop()
+
+        for index in range(10):
+            loop.start_soon(child(index))
+
+        loop.run()
+
+        self.assertEqual(order, [index for index in range(10)])
+
+if __name__ == '__main__':
+    main()
+
