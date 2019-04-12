05f68f6bb73d5530bd4cf0237078e4072a8eb690
Adontz <adontz@gmail.com>
2019-04-12 14:33:03 +0400

    Add nested nursery cancelation test.

--- tests/nursery_test.py
+++ tests/nursery_test.py
@@ -19,6 +19,7 @@ class TestNursery(TestCase):
     def test_exception_policy(self):
         async def child(x, y):
             z = x / y
+            del z
 
         async def parent(vars, exception_policy):
             try:
@@ -77,6 +78,57 @@ class TestNursery(TestCase):
         loop.run()
         self.assertEqual(vars['count_enter'], 2)
         self.assertEqual(vars['count_exit'], 1)
+
+    def test_nested_exception_handling(self):
+        # Timeline without interruption
+        #      0.0 0.1 0.2 0.3 0.4 0.5 0.6
+        # 1a    |***|***|***|***!***|   |
+        # 1a/2a |   |***|***|***!***|   |
+        # 1a/2b |   |***|   |   !   |   |
+        # 1b    |   |***|***|***!***|***|
+        # 1b/2c |   |   |***|***!***|***|
+        # 1b/2c |   |   |***|   !   |   |
+        # Execution will be interrupted at 0.4s,
+        # thus there should be 6 ins and 2 outs.
+        async def child2(vars, delay):
+            vars['count_enter'] += 1
+            await sleep(delay)
+            vars['count_exit'] += 1
+
+        async def child1(vars):
+            vars['count_enter'] += 1
+
+            async with Nursery() as nursery:
+                await sleep(0.1)
+                await nursery.start_soon(child2(vars, 0.4))
+                await nursery.start_soon(child2(vars, 0.1))
+
+            vars['count_exit'] += 1
+
+        async def child0():
+            await sleep(0.4)
+            raise Exception("STOP ALL")
+
+        async def parent(vars):
+            vars['count_enter'] = 0
+            vars['count_exit'] = 0
+
+            try:
+                async with Nursery() as nursery:
+                    await nursery.start_soon(child0())
+                    await nursery.start_soon(child1(vars))
+                    await sleep(0.1)
+                    await nursery.start_soon(child1(vars))
+            except Exception:
+                pass
+
+
+        vars = {}
+        loop = Loop()
+        loop.start_soon(parent(vars))
+        loop.run()
+        self.assertEqual(vars['count_enter'], 6)
+        self.assertEqual(vars['count_exit'], 2)
 
 
 if __name__ == '__main__':
