78ab56ebf0b8e3561d0b8691b12a397e049aca6e
Adontz <adontz@gmail.com>
2019-04-19 22:12:51 +0400

    Add socket.shutdown test.

--- tests/socket_test.py
+++ tests/socket_test.py
@@ -79,8 +79,7 @@ class TestSocket(TestCase):
         async def server_listen():
             server_socket = socket()
             server_socket.reuse_addr = True
-            server_socket.reuse_port = True
-            server_socket.bind(('127.0.0.1', 33333))
+            server_socket.bind(('127.0.0.1', 65533))
             await server_socket.listen(1)
 
             async with Nursery() as nursery:
@@ -91,9 +90,41 @@ class TestSocket(TestCase):
         async def client_connect():
             client_socket1 = socket()
             await sleep(0.01) # Make client start later, when server already listens.
-            await client_socket1.connect(('127.0.0.1', 33333))
+            await client_socket1.connect(('127.0.0.1', 65533))
             await client_socket1.send(b'A')
             self.assertEqual(await client_socket1.recv(1), b'B')
+            await client_socket1.close()
+
+        loop = Loop()
+        loop.start_soon(server_listen())
+        loop.start_soon(client_connect())
+        loop.run()
+
+    def test_socket_shutdown(self):
+        async def server_client_handler(client_socket2, client_address):
+            await client_socket2.shutdown()
+
+            with self.assertRaises(BrokenPipeError):
+                await client_socket2.send(b'B')
+
+            await client_socket2.close()
+
+        async def server_listen():
+            server_socket = socket()
+            server_socket.reuse_addr = True
+            server_socket.bind(('127.0.0.1', 65532))
+            await server_socket.listen(1)
+
+            async with Nursery() as nursery:
+                await server_socket.accept(nursery, server_client_handler)
+
+            await server_socket.close()
+
+        async def client_connect():
+            client_socket1 = socket()
+            await sleep(0.01) # Make client start later, when server already listens.
+            await client_socket1.connect(('127.0.0.1', 65532))
+            self.assertEqual(await client_socket1.recv(1), b'')
             await client_socket1.close()
 
         loop = Loop()
