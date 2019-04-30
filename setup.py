a28b932af8046e86e841911334a6b380c030d00c
adontz <adontz@gmail.com>
2019-04-30 21:36:46 +0400

    Update setup script. Generate version from last commit date.

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+from datetime import datetime
 from setuptools import find_packages
 from setuptools import setup
+from subprocess import run
 
 with open('README.rst', 'r') as readme_file:
     readme_text = readme_file.read()
@@ -9,17 +11,33 @@ with open('README.rst', 'r') as readme_file:
 with open('LICENSE') as license_file:
     license_text = license_file.read()
 
+commit_datetime = datetime.min
+
+git = run(['git', 'log', '--max-count=1', '--format=%ct'], capture_output=True)
+
+if git.returncode == 0:
+    commit_timestamp = int(git.stdout.strip(b'\n'))
+    commit_datetime = datetime.utcfromtimestamp(commit_timestamp)
+
+version = f'0.2.{commit_datetime:%y%m%d}'
+
 setup(
     name='broomio',
-    version='0.0.1',
+    version=version,
     author='Roman Akopov',
     author_email='adontz@gmail.com',
-    description='Broomio',
+    # maintainer='',
+    # maintainer_email='',
+    url='https://github.com/triflesoft/broomio',
     license=license_text,
+    description='Broomio',
     long_description=readme_text,
     long_description_content_type='text/markdown',
-    url='https://github.com/triflesoft/broomio',
-    packages=find_packages(exclude=('docs', 'examples', 'tests')),
+    # keywords='',
+    # platforms='',
+    # fullname='',
+    # contact='',
+    # contact_email='',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -30,5 +48,9 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
+    # download_url='',
+    # provides='',
+    # requires='',
+    # obsoletes='',
+    packages=find_packages(exclude=('docs', 'examples', 'tests')),
 )
-
